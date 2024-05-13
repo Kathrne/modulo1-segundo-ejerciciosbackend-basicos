e48a0fa8b8323af0e918a8bb5075b698ba827440
Michelle <michelle.linars@outlook.com>
2024-05-13 15:28:13 -0600

    Ejercicio7

--- ejercicios/ejercicio7.js
+++ ejercicios/ejercicio7.js
@@ -1,16 +1,11 @@
 /*  Ordena un arreglo de objetos según una propiedad específica, como la edad o el precio. */
 
-// Arreglo de objetos
-const productos = [
-    { nombre: "Camisa", precio: 25 },
-    { nombre: "Pantalón", precio: 30 },
-    { nombre: "Calcetines", precio: 10 },
-    { nombre: "Abrigo", precio: 50 }
-];
+let persona = [
+    {nombre:"Damaris", edad :24 },
+    {nombre:"Nixon", edad :26 },
+    {nombre:"Eli", edad :23 },
+    {nombre:"Samuel", edad :27 }
+]
 
-// Ordenar el arreglo de objetos según la propiedad 'precio'
-productos.sort((a, b) => a.precio - b.precio);
-
-// Mostrar el arreglo ordenado
-console.log("Arreglo ordenado por precio:");
-console.log(productos);
+let PersonasOrdenadas = persona.sort((a, b) => a.edad -b.edad)
+console.log(PersonasOrdenadas);
